18dc67f6ee1d8ced61d8fd79f36de3ca6adef9f7
serega.starshov <serega.starshov@mail.ru>
2021-08-17 03:15:00 +0300

    add subLesson

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 'use strict';
 
-function DomElement(selector, height, width, background, fontSize) {
+function DomElement(selector, height, width, background, fontSize, position, top, right, left, down) {
     this.selector = selector;
     this.height = height;
     this.width = width;
     this.bg = background;
     this.fontSize = fontSize;
+    this.position = position;
 }
 
 
@@ -15,35 +16,46 @@ DomElement.prototype.createElement = function() {
         elem = document.createElement('div');
         elem.classList.add('main');
         elem.style.cssText =`height: ${this.height};
-         width: ${this.width}; background: ${this.bg}; fontSize: ${this.fontSize};`;
+         width: ${this.width}; background: ${this.bg}; fontSize: ${this.fontSize};
+         position: ${this.position};`;
         elem.textContent = this.text;
+
+        
     } else if(this.selector.startsWith('#')) {
         elem = document.createElement('p');
         elem.setAttribute('id', 'main');
         elem.style.cssText =`height: ${this.height};
          width: ${this.width}; background: ${this.bg};
-          fontSize: ${this.fontSize}; font-family: ${this.fontFamily};`;
+          fontSize: ${this.fontSize}; font-family: ${this.fontFamily}; position: ${this.position};`;
         elem.textContent = this.text;
     }
 
     document.body.append(elem);
+    let left = 0,
+        top = 0,
+        right = 0,
+        down = 0;
+    document.addEventListener('keydown', (event) => {
+        
+        if(event.code === 'ArrowRight') {
+            left += 10;
+            elem.style.left = left + 'px';
+        } else if(event.code === 'ArrowLeft') {
+           left -= 10;
+            elem.style.left = left + 'px';
+        } else if (event.code === 'ArrowDown') {
+            top += 10;
+            elem.style.top = top + 'px';
+        } else if (event.code === 'ArrowUp') {
+            top -= 10;
+            elem.style.top = top + 'px';
+        }
+        
+    });
 };
 
-let newElement = new DomElement('.main', '250px', '850px', 'green', '32px');
-newElement.createElement();
-console.log(newElement);
+let newElement = new DomElement('.main', '100px', '100px', 'green', '32px', 'absolute');
 
 
-function NewElem(selector, height, width, background, fontSize, text, fontFamily) {
-    DomElement.apply(this, arguments);
-    this.text = text;
-    this.fontFamily = fontFamily;
-}
+document.addEventListener('DOMContentLoaded', newElement.createElement());
 
-NewElem.prototype = Object.create(DomElement.prototype);
-
-
-
-let objNewElem = new NewElem('#main', '140px', '450px', 'blue', '32px', 'Lorem Ipsum', 'Candara');
-console.log(objNewElem);
-objNewElem.createElement();
